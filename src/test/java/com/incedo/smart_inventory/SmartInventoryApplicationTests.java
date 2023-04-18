b62ad5c8334728e77442a4df120c9c2ebea2221e
achuthan01 <achuthan.r@incedoinc.com>
2023-04-18 22:11:41 +0530

    Initial commit

---
+++ src/test/java/com/incedo/smart_inventory/SmartInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incedo.smart_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
